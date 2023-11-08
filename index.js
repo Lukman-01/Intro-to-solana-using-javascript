113f9d8d9d60f1b1e515c8e22e6592f5f803edcb
Lukman Abdulyekeen <abdulyekeenlukman@gmail.com>
2023-11-08 10:47:58 +0000

    added pkey of user from cli

--- index.js
+++ index.js
@@ -3,35 +3,22 @@ const {
     Connection,       // This class provides API for interacting with the Solana JSON RPC endpoint
     PublicKey,        // Class to work with Solana public keys
     clusterApiUrl,    // Function to get the URL of various Solana clusters (devnet, testnet, mainnet-beta)
-    Keypair,          // Class to represent a keypair in Solana for signing transactions
+    //Keypair,          // Class to represent a keypair in Solana for signing transactions
     LAMPORTS_PER_SOL  // Constant to convert between lamports and SOL (1 SOL = 1 billion lamports)
 } = require("@solana/web3.js");
 
-// Generate a new keypair for a wallet
-const newPair = new Keypair();
-
-// Extract and convert the public key to a string for easy display
-const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-// Extract the private key; needed for signing transactions and to recover the keypair
-const privateKey = newPair._keypair.secretKey;
+// Import prompt module in order to prompt users for their public address
+const prompt = require('prompt-sync')();
 
 // Set up a connection to the Solana devnet
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-// Log the public key of the new keypair
-console.log("Public Key of the generated keypair", publicKey);
-
 // Function to check and log the wallet balance of the created keypair
-const getWalletBalance = async () => {
+const getWalletBalance = async (userPublicKey) => {
     try {
-        // Log the connection object for debug purposes
-        console.log("Connection object is:", connection);
-
-        // Reconstruct a wallet from the private key
-        const myWallet = await Keypair.fromSecretKey(privateKey);
         // Fetch the balance of the wallet
         const walletBalance = await connection.getBalance(
-            new PublicKey(myWallet.publicKey)
+            new PublicKey(userPublicKey)
         );
         // Convert the balance from lamports to SOL and log it
         console.log(`Wallet balance: ${walletBalance / LAMPORTS_PER_SOL} SOL`);
@@ -42,16 +29,14 @@ const getWalletBalance = async () => {
 };
 
 // Function to airdrop SOL into the created wallet
-const airDropSol = async () => {
+const airDropSol = async (userPublicKey) => {
     try {
-        // Reconstruct the wallet from the saved private key
-        const myWallet = await Keypair.fromSecretKey(privateKey);
-
+         
         // Log the initiation of the airdrop
         console.log("Airdropping some SOL to my wallet!");
         // Request an airdrop of 2 SOL to the wallet
         const fromAirDropSignature = await connection.requestAirdrop(
-            new PublicKey(myWallet.publicKey),
+            new PublicKey(userPublicKey),
             2 * LAMPORTS_PER_SOL
         );
         // Confirm the transaction of the airdrop
@@ -62,14 +47,19 @@ const airDropSol = async () => {
     }
 };
 
+// Prompt user for public key
+const userPublicKey = prompt('Please enter your solana public key: ');
+
+// sample address use for testing: dDCQNnDmNbFVi8cQhKAgXhyhXeJ625tvwsunRyRc7c8
+
 // Main function to demonstrate the balance before and after an airdrop
 const mainFunction = async () => {
     // Check balance before airdrop
-    await getWalletBalance();
+    await getWalletBalance(userPublicKey);
     // Perform airdrop
-    await airDropSol();
+    await airDropSol(userPublicKey);
     // Check balance after airdrop to see the change
-    await getWalletBalance();
+    await getWalletBalance(userPublicKey);
 }
 
 // Execute the main function
